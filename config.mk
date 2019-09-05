e28a4df590ec77832e5ae03c74bec87e0d3b5b30
Robin Brandt <Robinbrandt@outlook.de>
2019-09-05 20:27:03 +0200

    bla

--- config.mk
+++ config.mk
@@ -5,4 +5,4 @@ PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger 
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=917eeaf26bca5342817419d0c360301896932476
 SET_BUILDBOT=env
-MAKE_ARGS=V=s
+
